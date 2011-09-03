70ecf77f372a6a1301e712feb0cb0ceaad8bea59
Louis Taylor <kragniz@gmail.com>
2011-09-03 18:20:03 +0100

    internet class working

--- libpychi/internet.py
+++ libpychi/internet.py
@@ -1,16 +1,26 @@
+import urllib2
 class Internet(object):
     def __init__(self):
-        commonHosts = ('http://www.google.com',
-                       'http://www.gnome.org',
-                       'http://www.debian.org')
+        self.commonHosts = ('http://www.google.com',
+                            'http://www.gnome.org',
+                            'http://www.debian.org')
     
-    def _checkHost(self, host):
-        pass
+    def _checkHost(self, url):
+        try:
+            urllib2.urlopen(url)
+            return True
+        except:
+            return False
     
     def connected(self):
-        return False
+        isConnected = True
+        for url in self.commonHosts:
+            isConnected = (self._checkHost(url) and isConnected)
+        return isConnected
     
 if __name__ == '__main__':
     net = Internet()
     if net.connected():
-        print('we are connected to the internet!')
+        print('We are connected to the internet!')
+    else:
+        print('Oh noes! No interwebs!')
