0ce7079fad6b95ec481342950b66ba7a7f1e83a5
Louis Taylor <kragniz@gmail.com>
2011-09-04 00:16:22 +0100

    started the applet

--- libpychi/data.py
+++ libpychi/data.py
@@ -1,2 +1,4 @@
 DESCRIPTION = '''Check if this machine is connected to the world wide web'''
 VERSION = '0.001-alpha'
+CONNECTED_ICON = '/usr/share/pixmaps/pychi/internet-connected.svg'
+DISCONNECTED_ICON = '/usr/share/pixmaps/pychi/internet-disconnected.svg'
