51d3253ed79c4b19e0612c8c21a4b6cd3fb14c2b
Louis Taylor <kragniz@gmail.com>
2011-09-04 01:18:49 +0100

    made the applet work a bit better, added menu

--- libpychi/data.py
+++ libpychi/data.py
@@ -1,6 +1,24 @@
 NAME = 'pychi'
-DESCRIPTION = '''Check if this machine is connected to the world wide web'''
+DESCRIPTION = '''py can has internet - Check if this machine is connected to the world wide web'''
 VERSION = '0.001-alpha'
 ICON_DIR = '/usr/share/pixmaps/pychi/'
 CONNECTED_ICON = 'internet-connected.svg'
 DISCONNECTED_ICON = 'internet-disconnected.svg'
+
+COPYRIGHT = '(c) 2011 Louis Taylor'
+AUTHORS = ['Louis Taylor']
+LICENCE = '''Check if this machine is connected to the world wide web
+Copyright (C) 2011  Louis Taylor
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
